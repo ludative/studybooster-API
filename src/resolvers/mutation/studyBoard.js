d5c41615de338a46cc0e7eb76d3570ce6f3774cb
cok2exe <cok2exe@gmail.com>
2019-09-15 16:48:17 +0900

    add :) delete one study board

--- src/resolvers/mutation/studyBoard.js
+++ src/resolvers/mutation/studyBoard.js
@@ -1,13 +1,27 @@
 import models from "../../models";
 
+// 게시판 글 생성
 const createStudyBoard = async (_, { params }) => {
   const studyBoard = await models.StudyBoard.create(params);
 
   return studyBoard;
 };
 
+// 게시판 글 삭제
+const deleteStudyBoard = async (_, { id }) => {
+  await models.StudyBoard.destroy({
+    where: { id }
+  });
+
+  // 삭제 후 목록 리턴
+  const studyBoards = await models.StudyBoard.findAndCountAll();
+
+  return studyBoards;
+};
+
 const studyBoardMutations = {
-  createStudyBoard
+  createStudyBoard,
+  deleteStudyBoard
 };
 
 export default studyBoardMutations;
