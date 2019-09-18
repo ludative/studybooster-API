aeb3c1666c84325eb2029666cae44d09604b44a4
cok2exe <cok2exe@gmail.com>
2019-09-18 11:09:18 +0900

    add :) 필요할지 모르겠지만 category 별로 게시판 불러오는 것 추가

--- src/resolvers/query/studyBoard.js
+++ src/resolvers/query/studyBoard.js
@@ -7,16 +7,26 @@ const getStudyBoards = async () => {
   return studyBoards;
 };
 
-// 스터디 상세 가져오기
+// 스터디 게시판 상세 가져오기
 const getStudyBoardById = async (_, { id }) => {
   const studyBoard = await models.StudyBoard.findByPk(id);
 
   return studyBoard;
 };
 
+// 카테고리 별 스터디 게시판 목록 가져오기
+const getStudyBoardsByCategory = async (_, { category }) => {
+  const studyBoards = await models.StudyBoard.findAndCountAll({
+    where: { category }
+  });
+
+  return studyBoards;
+};
+
 const studyBoardQuery = {
   getStudyBoards,
-  getStudyBoardById
+  getStudyBoardById,
+  getStudyBoardsByCategory
 };
 
 export default studyBoardQuery;
