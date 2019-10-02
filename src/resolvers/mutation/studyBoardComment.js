45f9a39774e1686c268c484e8396286e4b0cad12
cok2exe <cok2exe@gmail.com>
2019-10-02 14:50:40 +0900

    add :) delete comment

--- src/resolvers/mutation/studyBoardComment.js
+++ src/resolvers/mutation/studyBoardComment.js
@@ -18,8 +18,21 @@ const createStudyBoardComment = async (
   return studyBoardComment;
 };
 
+// 게시판 댓글 삭제
+const deleteStudyBoardComment = async (_, { id }, context) => {
+  const user = context.user;
+  const studyBoardComment = await models.StudyBoardComment.findByPk(id);
+  if (studyBoardComment.UserId !== user.id)
+    throw new Error("본인의 댓글만 삭제할 수 있습니다.");
+
+  await studyBoardComment.destroy();
+
+  return { isSuccess: true };
+};
+
 const studyBoardCommentMutations = {
-  createStudyBoardComment: authenticatedMiddleware(createStudyBoardComment)
+  createStudyBoardComment: authenticatedMiddleware(createStudyBoardComment),
+  deleteStudyBoardComment: authenticatedMiddleware(deleteStudyBoardComment)
 };
 
 export default studyBoardCommentMutations;
