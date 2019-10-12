def33b35e897ebbbfe4dc322e3e09c9c0d87a2f8
Saemii <wrfg12@gmail.com>
2019-10-12 12:26:08 +0900

    update :) getStudies query(스터디 리스트 검색포함)

--- src/resolvers/query/studies.js
+++ src/resolvers/query/studies.js
@@ -1,8 +1,9 @@
 import { Op } from "sequelize";
 import models from "../../models";
+import calculatePagination from "../../utils/calculatePagination";
 
 // 스터디 리스트
-const getStudies = async ({ params, userId }) => {
+const getStudies = async (_, { paginationParams, params }) => {
   const {
     name,
     description,
@@ -16,11 +17,13 @@ const getStudies = async ({ params, userId }) => {
     minLimitedMemberCount,
     maxLimitedMemberCount,
     isPrivate,
+    UserId,
     orderBy = "createdAt",
     orderDirection = "DESC"
   } = params;
 
   const where = {};
+  const order = [[orderBy, orderDirection]];
 
   /**
    * like 검새기 가능한 컬럼
@@ -39,25 +42,36 @@ const getStudies = async ({ params, userId }) => {
 
   /**
    * 일치 검색기 가능한 컬럼
-   * weekPeriod, startDate, endDate, startTime, endTime,
+   * weekPeriod,
    */
-
-  if (typeof startDate === "float") where.startDate = startDate;
-  if (typeof endDate === "float") where.endDate = endDate;
-  if (typeof startTime === "integer") where.startTime = startTime;
-  if (typeof endTime === "integer") where.endTime = endTime;
+  if (typeof UserId === "integer") where.UserId = UserId;
   if (typeof weekPeriod === "integer") where.weekPeriod = weekPeriod;
 
   /**
    * 범위 검새기 가능한 컬럼
-   * limitedMemberCount
+   * startDate, endDate, startTime, endTime, limitedMemberCount
    */
+  if (typeof startDate === "float") where.startDate = { [Op.gte]: startDate };
+  if (typeof endDate === "float") where.endDate = { [Op.lte]: endDate };
+  if (typeof startTime === "integer") where.startTime = { [Op.gte]: startTime };
+  if (typeof endTime === "integer") where.endTime = { [Op.lte]: endTime };
   if (typeof minLimitedMemberCount === "integer")
     where.limitedMemberCount = {
-      [Op.lte]: minLimitedMemberCount
+      [Op.gte]: minLimitedMemberCount
     };
 
-  const studies = await models.Study.findAndCountAll();
+  if (typeof maxLimitedMemberCount === "integer")
+    where.limitedMemberCount = {
+      [Op.lte]: maxLimitedMemberCount
+    };
+
+  if (typeof isPrivate === "boolean") where.isPrivate = isPrivate;
+
+  const studies = await models.Study.findAndCountAll({
+    where,
+    order,
+    ...calculatePagination({ ...paginationParams })
+  });
 
   return studies;
 };
@@ -68,3 +82,10 @@ const getStudyById = async (_, { id }) => {
 
   return study;
 };
+
+const studyQuery = {
+  getStudies,
+  getStudyById
+};
+
+export default studyQuery;
