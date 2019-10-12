01de21211b9fcb46ef309b240ddc8c166ea20e8d
Saemii <wrfg12@gmail.com>
2019-10-12 13:49:23 +0900

    update :) deleteStudy 추가

--- src/resolvers/mutation/studies.js
+++ src/resolvers/mutation/studies.js
@@ -1,14 +1,69 @@
 import models from "../../models";
+import { authenticatedMiddleware } from "../../utils/middleware";
 
-const createStudy = async (_, {params}) => {
-    const study = await models.Study.create(params, {
-        include: [models.StudyDay]
+const createStudy = async (_, { params }) => {
+  const study = await models.Study.create(params, {
+    include: [models.StudyDay]
+  });
+  return study;
+};
+
+const deleteStudy = async (_, { id }, context) => {
+  try {
+    const user = context.user;
+    const study = await models.Study.findByPk(id);
+
+    if (study.UserId !== user.id)
+      throw new Error("스터디 관리자만 스터디 삭제가 가능합니다.");
+
+    // 1. StudyDay 삭제하기
+    const studyDays = await models.StudyDay.findAll({
+      where: { StudyId: id }
     });
-    return study;
+    // 2, StudyBoard 삭제하기
+    const studyBoards = await models.StudyBoard.findAll({
+      where: { StudyId: id }
+    });
+    // 3. StudyMember 삭제하기
+    const studyMembers = await models.StudyMember.findAll({
+      where: { StudyId: id }
+    });
+    // 4. StudyActionLog 삭제하기
+    const studyActionLogs = await models.StudyActionLog.findAll({
+      where: { StudyId: id }
+    });
+    // 5. StudyBookmark 삭제하기
+    const studyBookmarks = await models.StudyBookmark.findAll({
+      where: { StudyId: id }
+    });
+
+    await Promise.all(
+      studyDays.map(day => models.StudyDay.destroy({ where: { id: day.id } })),
+      studyBoards.map(board =>
+        models.StudyBoard.destroy({ where: { id: board.id } })
+      ),
+      studyMembers.map(member =>
+        models.StudyMember.destroy({ where: { id: member.id } })
+      ),
+      studyActionLogs.map(actionLog =>
+        models.StudyActionLog.destroy({ where: { id: actionLog.id } })
+      ),
+      studyBookmarks.map(bookmark =>
+        models.StudyBookmark.destroy({ where: { id: bookmark.id } })
+      )
+    );
+
+    await study.destroy();
+
+    return { isSuccess: true };
+  } catch (error) {
+    return { isSuccess: false };
+  }
 };
 
 const studyMutations = {
-    createStudy
+  createStudy,
+  deleteStudy: authenticatedMiddleware(deleteStudy)
 };
 
 export default studyMutations;
