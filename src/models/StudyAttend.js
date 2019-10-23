543ff906b83dd5eb9c1d7c2ca3a2349720ebdf36
Saemii <wrfg12@gmail.com>
2019-10-23 21:21:31 +0900

    add :) 스터디 출석 모델 생성

---
+++ src/models/StudyAttend.js
@@ -0,0 +1,13 @@
+export default (sequelize, DataTypes) => {
+  return sequelize.define("StudyAttend", {
+    confirmed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      comment: "출석 승인여부"
+    },
+    confirmedDate: {
+      type: DataTypes.INTEGER(13),
+      comment: "출석 승인날짜"
+    }
+  });
+};
