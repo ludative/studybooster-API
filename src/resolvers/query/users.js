ed8004b4ebd554c6abbcd5e6cb07772efc5790a5
Saemii <wrfg12@gmail.com>
2020-01-28 21:48:04 +0900

    update :) getUserByToken api 추가

--- src/resolvers/query/users.js
+++ src/resolvers/query/users.js
@@ -1,20 +1,21 @@
-import {Op} from 'sequelize';
+import { Op } from "sequelize";
 import models from "../../models";
 import calculatePagination from "../../utils/calculatePagination";
+import { authenticatedMiddleware } from "../../utils/middleware";
 
-const getUsers = async (_, {paginationParams, searchingText = ''}) => {
+const getUsers = async (_, { paginationParams, searchingText = "" }) => {
   const where = {};
 
   if (searchingText) {
     where[Op.or] = {
-        email: { [Op.like]: `%${searchingText}%` },
-        nickname: { [Op.like]: `%${searchingText}%` }
+      email: { [Op.like]: `%${searchingText}%` },
+      nickname: { [Op.like]: `%${searchingText}%` }
     };
   }
 
   return await models.User.findAndCountAll({
     where,
-    ...calculatePagination({...paginationParams})
+    ...calculatePagination({ ...paginationParams })
   });
 };
 
@@ -23,9 +24,14 @@ const getUserById = async (_, { id }) => {
   return user;
 };
 
+const getUserByToken = async (_, args, { user }) => {
+  return await models.User.findByPk(user.id);
+};
+
 const usersQuery = {
   getUsers,
-  getUserById
+  getUserById,
+  getUserByToken: authenticatedMiddleware(getUserByToken)
 };
 
 export default usersQuery;
